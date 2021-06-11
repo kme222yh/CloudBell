1cd2a325da7a56d2af371ee413541b05b2acdbd9
YuhiKamae <>
2021-06-11 23:35:23 +0900

    home usage 追加

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -1,10 +1,34 @@
 <template>
-<div>
-    <p>ホームだよ！！！！</p>
+<div id="usage">
+    <p>使い方</p>
+    <p>① Plansからメッセージスケジュールを作成</p>
+    <p>② Calendarからお好みの日付にスケジューを割り当て</p>
+    <p>③ Lineから{{bot_id}}を友達登録するとメッセージがくるよ！</p>
 </div>
 </template>
 
 <script>
     export default {
+        data: ()=>({
+            bot_id: '',
+        }),
+        mounted(){
+            this.bot_id = document.getElementsByName('bot_id')[0].content;
+        },
     }
 </script>
+
+<style lang="scss">
+#usage{
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    padding: 10px;
+
+    p{
+        margin-top: 40px;
+    }
+}
+</style>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
         <title>@php echo env('APP_NAME'); @endphp</title>
         <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
         <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="bot_id" content="@php echo env('LINE_BOT_ID'); @endphp">
     </head>
     <body>
         <header><div>@yield('header')</div></header>
