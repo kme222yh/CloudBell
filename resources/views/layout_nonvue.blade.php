88576ffc4c8337c9c955f65086bb61a6e2af1d8b
kme222yh <reuhu222@gmail.com>
2021-06-16 01:57:50 +0900

    add twitter meta

--- resources/views/layout_nonvue.blade.php
+++ resources/views/layout_nonvue.blade.php
@@ -6,6 +6,15 @@
         <link rel="icon" type="image/png" href="{{asset('favicon.png')}}">
         <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
         <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@php echo env('TWITTER_ID'); @endphp" />
+        <meta name="twitter:creator" content="@php echo env('TWITTER_ID'); @endphp" />
+        <meta property="og:url" content="@php echo env('APP_URL'); @endphp" />
+        <meta property="og:title" content="@php echo env('APP_NAME'); @endphp" />
+        <meta property="og:description" content="@php echo env('TWITTER_DESCRIPTION'); @endphp" />
+        <meta property="og:image" content="{{ asset('twitter.png') }}" />
+
     </head>
     <body>
         <header><div>@yield('header')</div></header>
